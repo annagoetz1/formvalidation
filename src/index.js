df9d857d420ba192042fed7163232b19c18b8c4f
annagoetz1 <annalgoetz1@gmail.com>
2024-09-30 15:22:56 -0400

    Add  name validation to trim input & check if empty

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission behavior
+
+
+  let valid = true;
+  
+    // Name validation
+const nameInput = document.getElementById('name');
+const nameError = document.getElementById('nameError');
+if (nameInput.value.trim() === "") {
+  nameError.style.display = 'block'; // Show error if name is empty
+  valid = false; // Set valid to false because the form has an error
+} else {
+  nameError.style.display = 'none'; // Hide error if the name is valid
+}
