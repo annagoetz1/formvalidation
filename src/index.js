4a6eddb378d56996d0bef1ea378a00035c539cef
annagoetz1 <annalgoetz1@gmail.com>
2024-09-30 15:35:32 -0400

    Add webpack, import styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+
+import './styles.css';
+
 document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission behavior
 
